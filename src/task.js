3fd9e5c71425c693fdb819be403f6d8a3cd8fc7f
balance80 <nilsen@gmail.com>
2021-04-27 01:43:17 +0200

    Delete task.js

--- src/task.js
+++
@@ -1,9 +0,0 @@
-const Task = function (job) {
-    this.job = job
-    this.onFinished = () => null
-    this.onError = () => null
-    this.currentRetry = 0
-    this.maxRetries = 4
-}
-
-module.exports = Task
